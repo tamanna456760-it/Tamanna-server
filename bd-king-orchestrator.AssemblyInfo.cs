b4dc3a115a571c9751bf77c6edbc1d6907f0f7cb
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-05-02 16:38:06 +0400

    Add files via upload

--- bd-king-orchestrator.AssemblyInfo.cs
+++ bd-king-orchestrator.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("bd-king-orchestrator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d3621459e2193cff549564b6fd3269f0147774")]
 [assembly: System.Reflection.AssemblyProductAttribute("bd-king-orchestrator")]
